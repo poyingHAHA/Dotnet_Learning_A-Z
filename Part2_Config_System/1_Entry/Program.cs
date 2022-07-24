d9177b371db67c5a80400e222324b5fc39aca636
poyingHAHA <poying.code@gmail.com>
2022-07-24 08:35:04 +0800

    1_config文件傳統讀取方法

--- Part2_Config_System/1_Entry/Program.cs
+++ Part2_Config_System/1_Entry/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+namespace _1_Entry
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile(@".\Dotnet_learning_A-Z\Part2_Config_System\1_Entry\config.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configRoot = configBuilder.Build();
+            string name = configRoot["name"];
+            System.Console.WriteLine($"name: {name}");
+            string address = configRoot.GetSection("proxy:address").Value;
+            System.Console.WriteLine($"address: {address}");
+        }
+    }
+
+    class Proxy
+    {
+        public string Address { get; set; }
+        public int Port { get; set; } // 由Binder自動做類型轉換
+    }
+}
